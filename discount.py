a18112d6daf7fe8a6b639db2d9d8a2b35844a0cf
muriukiian <muriukiian47@gmail.com>
2024-03-07 13:53:46 +0300

    plp assignment

---
+++ discount.py
@@ -0,0 +1,14 @@
+#Create a function named calculate_discount(price, discount_percent) that calculates the final price after applying a discount. The function should take the original price (price) and the discount percentage (discount_percent) as parameters. If the discount is 20% or higher, apply the discount; otherwise, return the original price.
+
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        discount_amount = price * (discount_percent / 100)
+        final_price = price - discount_amount
+        return final_price
+    else:
+        return  price
+    
+# Example usage:
+price = float(input('Enter price: '))
+discount_percent=float(input('Enter discount percent value: '))
+print(calculate_discount(price,discount_percent))
